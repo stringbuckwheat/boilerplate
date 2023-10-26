5de1792df95829457e1cd4584aeb127ec633cd32
stringbuckwheat <stringbuckwheat@gmail.com>
2023-10-26 17:04:18 +0900

    프로젝트 기본 세팅

---
+++ src/test/java/com/memil/setting/SettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memil.setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
